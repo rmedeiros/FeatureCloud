3cf36e3c718ca6da828fd719b4ae9f8a553452eb
Raul Medeiros <raul.medeiros@ehu.eus>
2019-05-27 10:41:28 +0200

    Version 0.0.1

--- src/main/java/com/onekin/tagcloud/utils/NewickUtils.java
+++ src/main/java/com/onekin/tagcloud/utils/NewickUtils.java
@@ -15,7 +15,7 @@ public class NewickUtils {
 			BufferedWriter writer = new BufferedWriter(new FileWriter("temp2.txt"));
 			writer.write(featureList);
 			writer.close();
-			p = Runtime.getRuntime().exec("python Treecloud.py");
+			p = Runtime.getRuntime().exec("python src/main/resources/Treecloud.py");
 			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			String ret = in.readLine();		
 			ret= ret.split("] ")[1].replaceAll("\'", "").toUpperCase();
