d82a31343fc420b1696208dcbd7b795ddf6a1e47
Raul Medeiros <raul.medeiros@ehu.eus>
2019-10-01 16:27:03 +0200

    Implemented controllers tests

---
+++ src/test/java/com/onekin/featurecloud/ReleaseDeltaServiceTest.java
@@ -0,0 +1,42 @@
+package com.onekin.featurecloud;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ReleaseDeltaServiceTest {
+
+
+    public void getFeaturesTest() {
+    }
+
+    public void getCoreAssetContentTest() {
+    }
+
+    public void getFeaturesFilteredTest() {
+    }
+
+    public void getDiffValuesTest() {
+    }
+
+    public void getNewickTreeTest() {
+    }
+
+    public void getModifiedFeaturesiblingsTest() {
+    }
+
+    public void getProductIdsTest() {
+    }
+
+
+    public void getFeaturesFilteredByProductAndPackageTest() {
+    }
+
+    public void getNewickTreeByProductTest() {
+    }
+
+    public void getComponentPackagesTest() {
+    }
+
+
+}
---
+++ src/test/java/com/onekin/featurecloud/SnapshotControllerTest.java
@@ -0,0 +1,49 @@
+package com.onekin.featurecloud;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = FeatureCloudApplication.class)
+@AutoConfigureMockMvc
+public class SnapshotControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void getFeatureTagCloud() throws Exception {
+        mockMvc.perform(get("/release/features/")
+                .contentType(MediaType.TEXT_HTML))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getFeatureVariationPoints() throws Exception {
+        mockMvc.perform(get("/release/features/nozzle_park_feature/")
+                .contentType(MediaType.TEXT_HTML))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getVariationPointContent() throws Exception {
+        mockMvc.perform(get("/features/nozzle_park_feature/asset/151/")
+                .contentType(MediaType.TEXT_HTML))
+                .andExpect(status().isOk());
+    }
+}
